c9f080268e2fe40f0e82820d85ca88f99c2c4762
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-03-04 10:44:59 -0300

    deixando código mais legível

--- beecrowd_3065.py
+++ beecrowd_3065.py
@@ -1,33 +1,33 @@
-j = 1
-total = 0
-sinal = []
+contador_de_casos = 1
+total_da_expressao = 0
+sinais_da_expressao = []
 
 while int(input()) != 0:
-    calc = input()
-    calc2 = calc.replace('+', ' ').replace('-', ' ').split()
+    calculo_para_ser_realizado = input()
+    calculo_sem_os_sinais = calculo_para_ser_realizado.replace('+', ' ').replace('-', ' ').split()
 
-    for m in range(len(calc2)):
-        calc2[m] = int(calc2[m])
+    for numero in range(len(calculo_sem_os_sinais)):
+        calculo_sem_os_sinais[numero] = int(calculo_sem_os_sinais[numero])
 
-    for i in range(len(calc)):
-        if calc[i] == '+':
-            sinal.append('+')
-        elif calc[i] == '-':
-            sinal.append('-')
+    for sinal in range(len(calculo_para_ser_realizado)):
+        if calculo_para_ser_realizado[sinal] == '+':
+            sinais_da_expressao.append('+')
+        elif calculo_para_ser_realizado[sinal] == '-':
+            sinais_da_expressao.append('-')
 
-    total = calc2[0]
+    total_da_expressao = calculo_sem_os_sinais[0]
 
-    k = 0
-    while k < len(sinal):
-        if sinal[k] == '+':
-            total += calc2[k+1]
-        elif sinal[k] == '-':
-            total -= calc2[k+1]
-        k += 1
+    posicao = 0
+    while posicao < len(sinais_da_expressao):
+        if sinais_da_expressao[posicao] == '+':
+            total_da_expressao += calculo_sem_os_sinais[posicao+1]
+        elif sinais_da_expressao[posicao] == '-':
+            total_da_expressao -= calculo_sem_os_sinais[posicao+1]
+        posicao += 1
 
-    print(f'Teste {j}')
-    print(total)
+    print(f'Teste {contador_de_casos}')
+    print(total_da_expressao)
     print()
-    j += 1
-    calc2 = []
-    sinal = []
+    contador_de_casos += 1
+    calculo_sem_os_sinais = []
+    sinais_da_expressao = []
